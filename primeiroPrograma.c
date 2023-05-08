158b901ef2dde6e75f7881db72f585741404bcae
Estilotabi <115471921+taironeAlves@users.noreply.github.com>
2023-05-07 21:17:53 -0300

    2117

--- primeiroPrograma.c
+++ primeiroPrograma.c
@@ -1,5 +1,8 @@
-/*
-Primeiro programa em C
-*/
-
 #include <stdio.h>
+
+/*Função main inicia a execuçã do programa*/
+
+int main(void){
+    printf("Bem Vindo Tairone Alves ao C!\n");
+    return 0; /* Indica que o programa terminou com sucesso*/
+}
